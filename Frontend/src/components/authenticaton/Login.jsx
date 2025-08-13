8f8c1f61a9e93add022ffc3ca85d0e4d208ef3a6
Esi-Twi <esitwi8@gmail.com>
2025-08-13 19:44:10 +0000

    created authentication pages

--- Frontend/src/components/authenticaton/Login.jsx
+++ Frontend/src/components/authenticaton/Login.jsx
@@ -3,9 +3,35 @@ import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div className='pages'>
-        <h1>Login</h1>
-        <Link to='/signup'>Signup</Link>
+    <div className='login-page'>
+      <div>
+        <img src='../../assets/images/suites/suite-7.jpg' />
+      </div>
+
+      <div className='login-inner'>
+        <h2>Log in to Exclusive </h2>
+        <p>Enter your details below</p>
+
+        <form>
+          <input type='text' placeholder='Email or Phone Number' />
+          <input type='password' placeholder='Password' />
+
+          <div className='login-div'>
+            <input type='button' className='login-button' value='Log In' />
+
+            <Link to=''>Forgot Passoword?</Link>
+          </div>
+        </form>
+
+        <Link to="" className='google-login'>
+          <img src='../../assets/icons/google-1.png'/>
+          <p>Sign up with Google</p>
+        </Link>
+
+        <p className='login-option'>Don't have an account? <Link to="/signup">Sign Up</Link></p>
+
+
+      </div>
     </div>
   )
 }
--- Frontend/src/components/authenticaton/Signup.jsx
+++ Frontend/src/components/authenticaton/Signup.jsx
@@ -3,10 +3,27 @@ import { Link } from 'react-router-dom'
 
 const Signup = () => {
   return (
-    <div className='pages'>
-        <h1>Signup</h1>
-        <Link to='/login'>Login</Link>
-    </div>
+     <div className='login-page'>
+            <div>
+              <img src='../../assets/images/suites/suite-7.jpg'/>
+            </div>
+    
+            <div className='login-inner'>
+              <h2>Create an account</h2>
+              <p>Enter your details below</p>
+    
+              <form>
+                <input type='text' placeholder='Name'/>
+                <input type='text' placeholder='Email or Phone Number'/>
+                <input type='password' placeholder='Password'/>
+    
+                <input type='button' className='signup-button' value='Create an account'/>
+              </form>
+    
+              <p className='login-option'>Already have an account? <Link to="/login">Log In</Link></p>
+    
+            </div>
+        </div>
   )
 }
 
