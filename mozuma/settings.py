5bfc09fb89c80b6cde8fecb424ce24d9b476d69c
Revic Osei Owusu <revicowusu@gmail.com>
2025-08-19 20:18:59 +0000

    Update settings.py

--- mozuma/settings.py
+++ mozuma/settings.py
@@ -112,8 +112,9 @@ DATABASES = {
         'NAME': 'mozuma_db',
         'USER': 'mozuma_user',
         'PASSWORD':os.environ.get('db_password'),
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'HOST': os.environ.get('db_host', '127.0.0.1'),
+        'PORT': os.environ.get('db_port', '3306'),
+    
     }
 }
 
