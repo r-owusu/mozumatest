fa484aa99ca649da8838d1f4f5e1dc28ff530846
Revic Osei Owusu <revicowusu@gmail.com>
2025-08-21 16:58:40 +0000

    Update settings.py

--- Backend/mozuma/settings.py
+++ Backend/mozuma/settings.py
@@ -180,6 +180,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
+import pymysql
+pymysql.install_as_MySQLdb()
+
+
+
 import os
 from dotenv import load_dotenv
 
