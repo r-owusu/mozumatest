fa9adb0b1d228777e1a0d1d3317ee09fb4fb0d9e
Revic Osei Owusu <revicowusu@gmail.com>
2025-08-22 20:55:10 +0000

    Update Booking.jsx1

--- Frontend/src/pages/Booking.jsx
+++ Frontend/src/pages/Booking.jsx
@@ -1,186 +1,91 @@
-import React from 'react'
-import suitesInfo from '../data/suitesInfo'
-
-
+import React from 'react';
+import suitesInfo from '../data/suitesInfo';
 
 const Booking = () => {
+
+  const getSuite = (id) => {
+    console.log("Selected suite ID:", id);
+    // You can add navigation or state logic here
+  };
+
   return (
     <div className='main-booking-page'>
-<<<<<<< HEAD
-      {/* ------booking details----------- */}
-      <div>
-        <div>
-          <div>
-            <img src='../../public/assets/images/events/wedding.png' />
-            <p>Guests</p>
-          </div>
-
-          <p>2 Adults, 0 Children</p>
-        </div>
-
-         <div>
-          <div>
-            <img src='../../public/assets/images/events/wedding.png' />
-            <p>Guests</p>
-          </div>
-
-          <p>2 Adults, 0 Children</p>
-        </div>
-
-         <div>
-          <div>
-            <img src='../../public/assets/images/events/wedding.png' />
-            <p>Guests</p>
-          </div>
-
-          <p>2 Adults, 0 Children</p>
-        </div>
-      </div>
-
-      {/* ------suites details----------- */}
-      <div>
-      {/* header */}
-      <div>
-        <h1>Availability</h1>   
-
-        <div>
-          <div>
-            <input type='checkbox'/>
-            <label>Suites</label>
-          </div>
-
-           <div>
-            <input type='checkbox'/>
-            <label>Tours</label>
-          </div>
-
-           <div>
-            <input type='checkbox'/>
-            <label>Events</label>
-          </div>
-        </div>     
-      </div>
-
-      {/* ---------suites list---------- */}
-       <div className='suites-list'>
-              {suitesInfo.map((suite) => (
-                <div className='suite-inner booking-page-suites' key={suite.id}>
-                  <div>
-                    <img className='suite-img' src={suite.img} />
-                  </div>
-      
-                  <div className='info'>
-                    <h1>{suite.name}</h1>
-      
-                    <div className='icons'>
-                      <div>
-                        <img src={suite.detailIcon1} />
-                        <p>{suite.detailMessage1}</p>
-                      </div>
-      
-                      <div>
-                        <img src={suite.detailIcon2} />
-                        <p>{suite.detailMessage2}</p>
-                      </div>
-                    </div>
-      
-                    <p className='suite-desc'>{suite.description}</p>
-      
-                    <div className='suite-button'>
-                      <a href='/booking' onClick={() => getSuite(suite.id)}>Book Now</a>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-      </div>
-
-=======
       <div className='booking-page-inner'>
-        {/* ------booking details----------- */}
+        {/* ------Booking Details----------- */}
         <div className='booking-details'>
           <div className='bdetails-main b1 border-left'>
-              <h1>
-              <img src='../../public/assets/images/suites/guests-icon.png' />
-                Guests
-              </h1>
-
+            <h1>
+              <img src='/assets/images/suites/guests-icon.png' alt="Guests" />
+              Guests
+            </h1>
             <p>2 Adults, 0 Children</p>
           </div>
 
           <div className='bdetails-main bdate border-left'>
-            <input type='date'/>
+            <input type='date' />
             <h1>Check In</h1>
           </div>
 
-           <div className='bdetails-main bdate'>
-            <input type='date'/>
+          <div className='bdetails-main bdate'>
+            <input type='date' />
             <h1>Check Out</h1>
           </div>
         </div>
 
-        {/* ------suites details----------- */}
-        <div>
-        {/* header */}
+        {/* ------Suites Details----------- */}
         <div className='booking-suites-details'>
-          <h1>Availability</h1>   
-
+          <h1>Availability</h1>
           <div className='suites-filter'>
             <div>
-              <input type='checkbox'/>
+              <input type='checkbox' />
               <label>Suites</label>
             </div>
-
             <div>
-              <input type='checkbox'/>
+              <input type='checkbox' />
               <label>Tours</label>
             </div>
-
             <div>
-              <input type='checkbox'/>
+              <input type='checkbox' />
               <label>Events</label>
             </div>
-          </div>     
+          </div>
         </div>
 
-        {/* ---------suites list---------- */}
+        {/* ---------Suites List---------- */}
         <div className='suites-list'>
-                {suitesInfo.map((suite) => (
-                  <div className='booking-page-suites' key={suite.id}>
-                    <div>
-                      <img className='suite-img' src={suite.img} />
-                    </div>
-        
-                    <div className='info'>
-                      <h1>{suite.name}</h1>
-        
-                      <div className='icons'>
-                        <div>
-                          <img src={suite.detailIcon1} />
-                          <p>{suite.detailMessage1}</p>
-                        </div>
-        
-                        <div>
-                          <img src={suite.detailIcon2} />
-                          <p>{suite.detailMessage2}</p>
-                        </div>
-                      </div>
-        
-                      <p className='suite-desc'>{suite.description}</p>
-        
-                      <div className='suite-button'>
-                        <a href='/booking' onClick={() => getSuite(suite.id)}>Book Now</a>
-                      </div>
-                    </div>
-                  </div>
-                ))}
+          {suitesInfo.map((suite) => (
+            <div className='booking-page-suites' key={suite.id}>
+              <div>
+                <img className='suite-img' src={suite.img} alt={suite.name} />
               </div>
+
+              <div className='info'>
+                <h1>{suite.name}</h1>
+
+                <div className='icons'>
+                  <div>
+                    <img src={suite.detailIcon1} alt="Icon1" />
+                    <p>{suite.detailMessage1}</p>
+                  </div>
+
+                  <div>
+                    <img src={suite.detailIcon2} alt="Icon2" />
+                    <p>{suite.detailMessage2}</p>
+                  </div>
+                </div>
+
+                <p className='suite-desc'>{suite.description}</p>
+
+                <div className='suite-button'>
+                  <button onClick={() => getSuite(suite.id)}>Book Now</button>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-
       </div>
->>>>>>> b73d966d561d21b998a6cc481dd9b5f94d98e4ce
     </div>
-  )
-}
+  );
+};
 
-export default Booking
+export default Booking;
